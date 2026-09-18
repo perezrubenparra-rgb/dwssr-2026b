44d33af5f3cf635c7cf9bea423806983ce4b64f9
perezrubenparra-rgb <perezrubenparra@gmail.com>
2026-09-18 18:14:13 +0000

    ✨ Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include itgam image

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
+//FUNCION PARA MANEJAR ERRORES
 var createError = require('http-errors');
+//IMPORTA EL FRAMEWORK ESPRESS
 var express = require('express');
+//IMPORTA MODULOS PARA MANEJAR RUTAS
 var path = require('path');
+//SIRVE PARA COOKES
 var cookieParser = require('cookie-parser');
+//NOS SIRVE PARA REGISTRAR TODO LO QUE SIRVE EN EL SERVIDOR
 var logger = require('morgan');
 
+// SE IMPORTN LAS RUTAS DE LA APLICACION
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//CREAR LA APLICACION EXPRESS
 var app = express();
 
-// view engine setup
+// CONFIGURA EL MOTOR DE VISTAS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// CONFIGURA EL USO DE MIDDLEWARES 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//CONFIGURACIONDE LA CARPETA DE ARCHIVOS ESTATICOS
 app.use(express.static(path.join(__dirname, 'public')));
 
+//REGISTRAMOS RUTAS
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// CAPTURAMOS LOS ERRORES COMO EL 
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// MANEJO DE ERRORES
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
